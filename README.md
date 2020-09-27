f38971426af2df8d899fbbaf0665470bfe9324f9
Beksultan Torokulov <beksultan_torokulov@epam.com>
2020-09-27 23:06:31 +0300

    added docker

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 #devices-list-client-server
+
+##Steps to start
+1. `git clone https://github.com/bekstoro/devices-list-client-server.git`
+2. `cd devices-list-client-server`
+3. `docker-compose up`
--- server/index.js
+++ server/index.js
@@ -1,42 +1,16 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const io = require('socket.io')();
+const http = require('http');
+const socketIO = require('socket.io');
 
 const devices = require('./devices');
 
-const app = express();
-const ioPort = 8000;
-const port = 8080;
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use(cors({
-  allowOrigin: ['*'],
-  allowedHeaders: ['*'],
-  methods: 'GET,POST,DELETE',
-  origin: ['http://localhost:3000'],
-}));
-
-app.get('/cart', (req, res) => res.json({cart: devices.getCart()}));
-
-app.post('/cart', (req, res) => res.json({cart: devices.addToCart(req.body)}));
-
-app.delete('/cart/:deviceId', (req, res) => res.json({cart: devices.removeFromCart(Number.parseInt(req.params && req.params.deviceId))}));
-
-app.get('/devices', (req, res) => res.json({devices: devices.getDevices()}));
-
-app.listen(port, () => {
-  console.log('Server is running on port 8080');
-});
-
-
-
-let interval;
+const port = 8000;
+let interval = null;
 let updateTime = devices.getUpdateTime();
 
-io.on('connection', (socket) => {
+function onSocketConnection(socket) {
   console.log('New client connected');
   if (interval) {
     clearInterval(interval);
@@ -44,13 +18,43 @@ io.on('connection', (socket) => {
   interval = setInterval(() => {
     if (updateTime !== devices.getUpdateTime()) {
       updateTime = devices.getUpdateTime();
-      socket.broadcast.emit('deviceUpdate', devices.getUpdateDevice());
+      const updateDevice = devices.getUpdateDevice();
+      if (updateDevice) {
+        socket.broadcast.emit('deviceUpdate', updateDevice);
+      }
     }
   }, 1000);
   socket.on('disconnect', () => {
     console.log('Client disconnected');
     clearInterval(interval);
   });
-});
-io.listen(ioPort);
-console.log('SocketIO is running on port 8000');
+}
+
+function startServer() {
+  const app = express();
+  const server = http.createServer(app);
+  const io = socketIO(server);
+
+  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.json());
+  app.use(cors({
+    allowOrigin: ['*'],
+    allowedHeaders: ['*'],
+    methods: 'GET,POST,DELETE',
+    origin: ['http://localhost', 'http://localhost:3000'],
+  }));
+
+  app.get('/cart', (req, res) => res.json({cart: devices.getCart()}));
+
+  app.post('/cart', (req, res) => res.json({cart: devices.addToCart(req.body)}));
+
+  app.delete('/cart/:deviceId', (req, res) => res.json({cart: devices.removeFromCart(Number.parseInt(req.params && req.params.deviceId))}));
+
+  app.get('/devices', (req, res) => res.json({devices: devices.getDevices()}));
+
+  io.on("connection", onSocketConnection);
+
+  server.listen(port, () => console.info(`Server is running on port ${port}`));
+}
+
+startServer();
